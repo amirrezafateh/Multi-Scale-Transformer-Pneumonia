ff4e47b5467a050f55676ffd5ce0a48582892223
amirrezafateh <155237074+amirrezafateh@users.noreply.github.com>
2024-06-21 01:47:33 +0330

    Add files via upload

---
+++ data/data_loader.py
@@ -0,0 +1,19 @@
+import os
+import torch
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+
+def get_data_loader(data_dir, batch_size):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # ResNet expects 224x224 input size
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    train_dataset = datasets.ImageFolder(root=os.path.join(data_dir, 'train'), transform=transform)
+    test_dataset = datasets.ImageFolder(root=os.path.join(data_dir, 'test'), transform=transform)
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    return train_loader, test_loader
