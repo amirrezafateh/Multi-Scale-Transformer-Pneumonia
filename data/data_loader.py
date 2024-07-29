b0cb27c6e33c1ef8328a73b3c68b0dfbc46cb046
amirrezafateh <155237074+amirrezafateh@users.noreply.github.com>
2024-07-29 20:36:31 +0330

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -4,18 +4,32 @@ from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 
 def get_data_loader(data_dir, batch_size):
-    transform = transforms.Compose([
-        transforms.Resize((512, 512)),  # ResNet expects 224x224 input size
+
+    # with data augmentation
+    train_transform = transforms.Compose([
+        transforms.Resize((512, 512)),
+        #transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(degrees=15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize((512, 512)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     
-    train_dataset = datasets.ImageFolder(root=os.path.join(data_dir, 'train'), transform=transform)
-    test_dataset = datasets.ImageFolder(root=os.path.join(data_dir, 'test'), transform=transform)
+    train_dataset = datasets.ImageFolder(root=os.path.join(data_dir, "train"), transform=train_transform)
+    test_dataset = datasets.ImageFolder(root=os.path.join(data_dir, 'test'), transform=test_transform)
     
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
     
+    
+
+
     return train_loader, test_loader
 
 
