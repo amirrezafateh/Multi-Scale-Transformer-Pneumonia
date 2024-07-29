3f043f17500ada40e71785f8f0b1eadbf41c6cfd
amirrezafateh <155237074+amirrezafateh@users.noreply.github.com>
2024-07-29 20:35:41 +0330

    Update MSR.py

--- model/MSR.py
+++ model/MSR.py
@@ -5,7 +5,7 @@ from .transformer import PositionEmbeddingSine
 from .transformer import Transformer
 
 class MSR(nn.Module):
-    def __init__(self, layers, num_classes=2, reduce_dim=64): 
+    def __init__(self, layers, num_classes=2, reduce_dim=256): 
         super(MSR, self).__init__()
         self.backbone = Backbone(
             'resnet{}'.format(layers), 
@@ -13,7 +13,8 @@ class MSR(nn.Module):
             return_interm_layers=True, 
             dilation=[False, True, True]
         )
-        self.embed=nn.Embedding(reduce_dim,1) 
+        self.embed_cat=nn.Embedding(reduce_dim,1) 
+        self.embed_3=nn.Embedding(reduce_dim,1) 
         self.pe_layer_cat=PositionEmbeddingSine(reduce_dim//2, normalize=True)
         self.pe_layer_3=PositionEmbeddingSine(reduce_dim//2, normalize=True)
 
@@ -50,46 +51,50 @@ class MSR(nn.Module):
 
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(reduce_dim*reduce_dim*2, num_classes)  # Assuming last feature map size
+        self.fc = nn.Linear(reduce_dim*2, reduce_dim)  # Assuming last feature map size
+        self.fc_2 = nn.Linear(reduce_dim, num_classes)  # Assuming last feature map size
         self.num_classes = num_classes
 
     def forward(self, x):
         # Backbone feature extraction
         back_x = self.backbone(x)
-
         red_back_1 = self.conv_red_1(back_x['1'])
         red_back_2 = self.conv_red_2(back_x['2'])
-
+        
         red_back_cat = torch.cat((red_back_1, red_back_2), dim=1)
         avg_back_cat = self.avgpool(red_back_cat)
-
+        
         red_back_3 = self.conv_red_3(back_x['3'])
         avg_back_3 = self.avgpool(red_back_3)
 
-
         masking=None
-        query_pos = self.embed.weight
+        query_pos = self.embed_cat.weight
 
         key_embed = red_back_cat
         query_embed = avg_back_cat.squeeze(-1)
         key_pos = self.pe_layer_cat(red_back_cat)
-        fg_embed_cat=self.transformer_cat(key_embed,masking,query_embed,query_pos,key_pos)
 
+        fg_embed_cat=self.transformer_cat(key_embed,masking,query_embed,query_pos,key_pos)
         
+        query_pos = self.embed_3.weight
         key_embed = red_back_3
         query_embed = avg_back_3.squeeze(-1)
         key_pos = self.pe_layer_3(red_back_3)
         fg_embed_3=self.transformer_3(key_embed,masking,query_embed,query_pos,key_pos)
+ 
 
-        out_back_cat = (torch.einsum("bchw,bcl->blhw",red_back_cat,fg_embed_cat)).permute(0, 2, 3, 1)
-        out_back_3 = (torch.einsum("bchw,bcl->blhw",red_back_3,fg_embed_3)).permute(0, 2, 3, 1)
-
+        #out_back_cat = (torch.einsum("bchw,bcl->blhw",red_back_cat,fg_embed_cat)).permute(0, 2, 3, 1)
+        #out_back_3 = (torch.einsum("bchw,bcl->blhw",red_back_3,fg_embed_3)).permute(0, 2, 3, 1)
+        
+        
         
         # Concatenate along the last dimension
-        out = torch.cat((out_back_cat, out_back_3), dim=-1)     
+        out = torch.cat((fg_embed_cat, fg_embed_3), dim=1)     
 
         out_1 = torch.flatten(out, 1)  # Flatten the feature maps
+        
         out_1 = self.fc(out_1)  # Fully connected layer
+        out_1 = self.fc_2(out_1)  # Fully connected layer
         return out_1
 
     def get_backbone_params(self):
